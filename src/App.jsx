4baa79c5e489510b8fbd1ce751d2cc097bc49fd2
Andrew Denta <andrew@denta.co>
2023-03-31 14:33:02 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import logo from "./assets/logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="mailto:press@healthworx.studio">
+          <img id="logo" src={logo} />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
